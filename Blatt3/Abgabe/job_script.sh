b72ac720cd8056115fc69c868249797735e43573
BJRWolf <benjaminwolf95@gmail.com>
2016-11-05 18:12:51 +0100

    Aufgabe1+2

---
+++ Blatt3/Abgabe/job_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH -N 4
+#SBATCH -n 16
+#SBATCH --ntasks-per-node 4
+#SBATCH --output timescript.out
+
+srun timescript.sh >> timescript.out
+echo fertig > job_script.out
---
+++ Blatt3/Abgabe/timescript.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "$(hostname), $(date +%Y-%m-%d\ %H:%M:%S.%N)"
