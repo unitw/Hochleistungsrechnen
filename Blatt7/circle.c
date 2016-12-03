54b270c7cc97b7139e7216707aae9a8d76ad7ddc
unitw <tw@ens-fiti.de>
2016-12-03 21:36:27 +0100

    änderungen

--- Blatt7/circle.c
+++ Blatt7/circle.c
@@ -1,116 +1,221 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#include <string.h>
 #include <mpi.h>
+   
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
-#define MASTER_PROCESS 0
-#define SEND_DATA_TAG 123
+#define ROOT 0
+#define TAG_INIT 0
+#define TAG_CIRCLE 1
+#define TAG_PRINT 2
+#define TAG_END_VALUE 3
 
-int*
-init (int part, int bufLength, int rest)
+typedef struct 
 {
   int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  
-  int* buf = malloc(sizeof(int) * bufLength);
-
-  srand(time(NULL) * rank);
-
-  for (int i = 0; i < part; i++)
-  {
-    buf[i] = rand() % 25; //do not modify % 25
-  }
-  
-  if (rank >= rest ) {
-	  buf[part] = -1;
-  }
-
-  return buf;
-}
-
-int*
-circle (int* buf)
-{
-  int rank, processCount;
+  int num_procs;
+  int moveSize;
+  int* bufsize;
   MPI_Status status;
-	
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &processCount);
-	
-  int sendID = (rank + 1) % processCount;
-  int recvID = rank == 0 ? processCount - 1 : rank - 1;
-	
-  MPI_Send( &buf, 1, MPI_INT, sendID, SEND_DATA_TAG, MPI_COMM_WORLD);
-  
-  MPI_Recv( &buf, 1, MPI_INT, recvID, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-  
+} CommParams;
+
+// In params müssen rank und num_procs gesetzt sein
+int* init (int completeSize, CommParams* params)
+{
+  int chunkSize = completeSize / params->num_procs;
+  int extra = completeSize % params->num_procs;
+  params->moveSize = MAX(1, chunkSize);
+
+  params->bufsize = malloc(sizeof(int) * params->num_procs);
+
+  for (int i = 0; i < params->num_procs; ++i)
+  {
+    params->bufsize[i] = chunkSize;
+    if ((i < extra) || (chunkSize == 0))
+    {
+      params->bufsize[i] += 1;
+    }
+  }
+
+  int N = params->bufsize[params->rank];
+  int* buf = malloc(sizeof(int) * N);
+
+  // mit -1 initialisieren
+  memset(buf, -1, sizeof(int) * N);
+
+  if (params->rank == ROOT)
+  {
+    int allData[completeSize];
+    srand(time(NULL));
+    for (int i = 0; i < completeSize; i++)
+    {
+      allData[i] = rand() % 25; //do not modify %25
+    }
+
+    int start = params->bufsize[ROOT];
+    for (int i = 1; i < params->num_procs; ++i)
+    {
+      int len = chunkSize;
+      if (i < extra)
+      {
+        len += 1;
+      }
+
+      MPI_Send(&allData[start], len, MPI_INT, i, TAG_INIT, MPI_COMM_WORLD);   
+      start += len;
+    }
+    memcpy(buf, allData, sizeof(int) * params->bufsize[ROOT]);
+  }
+  else
+  {
+    int len = chunkSize;
+    if (params->rank < extra)
+    {
+      len += 1;
+    }
+    MPI_Recv(buf, len, MPI_INT, ROOT, TAG_INIT, MPI_COMM_WORLD, &params->status);
+  }
+
   return buf;
 }
 
-int
-main (int argc, char** argv)
+int completeSize(int argc, char** argv)
 {
-  char arg[256];
-  int N;
-  int rank;
-  int processCount;
+  if (argc >= 2)
+  {
+    char arg[256];
+    sscanf(argv[1], "%s", arg);
+    return atoi(arg);
+  } 
+
+  return 0;
+}
+
+void printArray(int rank, int* buf, int len)
+{
+  for (int i = 0; i < len; ++i)
+  {
+    printf("rank %d:%d\n", rank, buf[i]);
+  }
+}
+
+void printComplete(int* buf, CommParams* params)
+{
+  if (params->rank == ROOT)
+  {
+    printArray(ROOT, buf, params->bufsize[ROOT]);
+    int procData[params->moveSize + 1];
+    for (int i = 1; i < params->num_procs; ++i)
+    {
+      MPI_Recv(procData, params->bufsize[i], MPI_INT, i, TAG_PRINT, MPI_COMM_WORLD, &params->status);
+      printArray(i, procData, params->bufsize[i]);
+    }
+  }
+  else
+  {
+    MPI_Send(buf, params->bufsize[params->rank], MPI_INT, ROOT, TAG_PRINT, MPI_COMM_WORLD);
+  }
+}
+
+void circle (int* buf, int endValue, CommParams* params)
+{
+  if (params->num_procs < 2)
+  {
+    return;
+  }
+
+  int myBufSize = params->bufsize[params->rank];
+  int moveSize = params->moveSize;
+
+  int sendTo = (params->rank + 1) % params->num_procs;
+  int recFrom = (params->rank -1) % params->num_procs;
+  int sendData[params->moveSize];
+
+  int endReached = 0;
+
+  while (1)
+  {
+    if (params->rank == (params->num_procs - 1) && buf[0] == endValue)
+    {
+      endReached = 1;
+    }
+    MPI_Bcast(&endReached, 1, MPI_INT, params->num_procs - 1, MPI_COMM_WORLD);
+
+    if (endReached)
+    {
+      break;
+    }
+
+    // die zu sendenden Werte aus dem Array kopieren
+    memcpy(sendData, &buf[myBufSize - moveSize], sizeof(int) * moveSize);
+    buf[myBufSize - 1] = buf[0];  // den Wert den wir eventuell behalten wollen ans Ende kopieren
+
+    if ((params->rank % 2) == 0)
+    {
+      MPI_Send(sendData, moveSize, MPI_INT, sendTo, TAG_CIRCLE, MPI_COMM_WORLD);
+      MPI_Recv(buf, moveSize, MPI_INT, recFrom, TAG_CIRCLE, MPI_COMM_WORLD, &params->status);
+    }
+    else
+    {
+      MPI_Recv(buf, moveSize, MPI_INT, recFrom, TAG_CIRCLE, MPI_COMM_WORLD, &params->status);
+      MPI_Send(sendData, moveSize, MPI_INT, sendTo, TAG_CIRCLE, MPI_COMM_WORLD);
+    }
+  }
+}
+
+int main (int argc, char** argv)
+{
   int* buf;
-  
-  //Initialisierung von mpi
+  int endValue;
+  CommParams params;
+
+  int size = completeSize(argc, argv);
+
+  if (size <= 0)
+  {
+    printf("Arguments error\n");
+    return EXIT_FAILURE;
+  }
+
   MPI_Init(&argc, &argv);
-  
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &processCount);
+  MPI_Comm_rank(MPI_COMM_WORLD, &params.rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &params.num_procs);
 
-  if (argc < 2)
+  buf = init(size, &params);
+
+  // dem letzten prozess mitteilen wann zu stoppen ist
+  if (params.rank == ROOT)
   {
-	  if (rank == MASTER_PROCESS)
-    {
-      printf("Arguments error!\n");
-    }
-	MPI_Finalize();
-    return EXIT_FAILURE;
+    MPI_Send(&buf[0], 1, MPI_INT, params.num_procs - 1, TAG_END_VALUE, MPI_COMM_WORLD);  
+  }
+  else if (params.rank == (params.num_procs - 1))
+  {
+    MPI_Recv(&endValue, 1, MPI_INT, ROOT, TAG_END_VALUE, MPI_COMM_WORLD, &params.status);
   }
 
-  sscanf(argv[1], "%s", arg);
-
-  // Array length
-  N = atoi(arg);
-  
-  if (processCount > N)
+  if (params.rank == ROOT)
   {
-	  if (rank == MASTER_PROCESS)
-    {
-      printf("Array size must be higher than Process count!\n");
-    }
-	MPI_Finalize();
-    return EXIT_FAILURE;
-  }
-  
-  int part = N / processCount;
-  int rest = N % processCount;
-  int bufLength = (rest != 0) ? part + 1 : part;
-  part = rank < rest ? part + 1 : part;
-  
-  //printf ("\nrank %d: %d\n", rank, part);
-  buf = init(part, bufLength, rest);
-
-  printf("\nBEFORE\n");
-
-  for (int i = 0; i < part; i++)
-  {
-    printf ("rank %d: %d\n", rank, buf[i]);
+    printf("\nBEFORE\n");
   }
 
-  circle(buf);
+  printComplete(buf, &params);
+  MPI_Barrier(MPI_COMM_WORLD);
 
-  printf("\nAFTER\n");
+  circle(buf, endValue, &params);
+  MPI_Barrier(MPI_COMM_WORLD);
 
-  for (int j = 0; j < part; j++)
+  if (params.rank == ROOT)
   {
-    printf ("rank %d: %d\n", rank, buf[j]);
+    printf("\nAFTER\n");
   }
+  printComplete(buf, &params);
 
   MPI_Finalize();
+  free(buf);
+  free(params.bufsize);
+
   return EXIT_SUCCESS;
 }
